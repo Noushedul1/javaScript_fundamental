9c6ecbaf20189fe0e1109ad20bb6906cce4e0f7f
Md.Noushedul Islam <noushedakib@gmail.com>
2020-10-18 21:16:31 +0600

    This is js fundamental code

---
+++ js/fundamental.js
@@ -0,0 +1,93 @@
+//console
+console.log("Hello noushedul islam akib");
+
+//js variable
+
+//null
+var cat = '';
+console.log(cat);
+
+//true ,false
+var gg = true;
+console.log(gg);
+
+var fls = false;
+document.write(fls);
+//intiger
+var a = 6664630;
+console.log(a);
+
+//float
+var b = 358.55;
+console.log(b);
+
+//string
+var c = "noushedul isalm akib";
+console.log(c);
+
+//alert
+alert("noushedul islam");
+
+//prompt
+prompt("Enter your name");
+
+//math
+var d = 10;
+var e = 20;
+console.log(d+e);
+
+var f = 20;
+var g = 10;
+console.log(f-g);
+
+var i = 10;
+var j = 10;
+console.log(i*j);
+
+var k = 10;
+var l = 2;
+console.log(k/l);
+
+//round,ceil,floor,pi
+var round = 10.3;
+console.log(Math.round(round));
+
+var ceil = 10.5;
+console.log(Math.ceil(ceil));
+
+console.log(Math.PI);
+
+//concatanation
+var first = "Noushedul";
+var second = "Islam";
+console.log(first+' '+second);
+
+//length
+var leng = "Southern University";
+console.log(leng.length);
+
+//typeof
+var dog = "tiger";
+console.log(typeof(dog));
+var int = 10;
+console.log(typeof(int));
+
+//type change
+var fnum = prompt("Enter first number: ");
+console.log(typeof(fnum));
+var typeChange =Number(fnum);
+console.log(typeChange+12);
+
+//toUpperCase, toLowerCase
+var upper = "noushedul islam";
+var upperCase = upper.toUpperCase();
+console.log(upperCase);
+
+var lower = "NOUSHEDUL ISALM";
+var lowerCase = lower.toLowerCase();
+console.log(lowerCase);
+
+//substring
+var subs = "southern";
+var subst = subs.substring(0,3);
+console.log(subst);
